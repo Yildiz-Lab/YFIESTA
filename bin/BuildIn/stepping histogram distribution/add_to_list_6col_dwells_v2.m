352897984b6261aaca74b505ea80a34201fb79a0
Joseph Slivka <slivka@Josephs-MacBook-Pro-2.local>
2024-03-07 10:35:18 -0800

    stepping rates using dt array (for MINFLUX)

--- bin/BuildIn/stepping histogram distribution/add_to_list_6col_dwells_v2.m
+++ bin/BuildIn/stepping histogram distribution/add_to_list_6col_dwells_v2.m
@@ -4,6 +4,9 @@ trace = varargin{1};
 use_thresh = varargin{2};
 dwells = varargin{3};
 time = varargin{4};
+if length(time) < 2
+    time = varargin{4}*0:length(trace(:,3))-1;
+end
 
 cp = trace(:,5);
 usage = trace(:,6);
@@ -14,13 +17,15 @@ start = 1;
 counter = length(dwells);
 for i = 2:limit
     if (cp(i) == 1)
-        dwell = start:i-1;
+        prev_start = start;
+        dwell = prev_start:i-1;
         start = i;
         if ( prod(usage(start:i)) >= use_thresh && start ~= 1)
             counter = counter+1;
             dwells(counter,1) = trace(i-1,4);
             dwells(counter,2) = trace(i-1,3);
-            dwells(counter,3) = length(dwell)*time;
+            % dwells(counter,3) = length(dwell)*time;
+            dwells(counter,3) = time(i-1)-time(prev_start); %JS Edit 2024/03/07
 
             if (trace(i-1,3) < -pi/2 || trace(i-1,3) > pi/2 )
                 dwells(counter,4) = -1;
