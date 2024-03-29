3324dd6b7d4fb3c0520c4b68cb28707ef4e3fb0e
yildi <jslivka@berkeley.edu>
2024-03-29 12:16:20 -0700

    small updates, polar plots, step stats

--- bin/BuildIn/polar plots/plot_polar_conversion.m
+++ bin/BuildIn/polar plots/plot_polar_conversion.m
@@ -1,11 +1,11 @@
-function plot_polar_conversion(r,theta,bindeg)
+function plot_polar_conversion(r,theta,binnum)
 
 % Pass in radius (+,- no issue)
 % Theta
 % bindeg - number of degrees per bin
 
 if nargin < 3
-    bindeg = 15;
+    binnum = 24;
 end
 
 % plot in cool spherical histogram fashion
@@ -14,8 +14,18 @@ subplot(1,2,1)
 polarscatter(theta,r)
 set(gca,'ThetaZeroLocation','top')
 
+
+% theta_transform = theta_transform - pi*(theta_transform > pi);
+fwd_mask = and(theta > -pi/2, theta < pi/2);
+
 subplot(1,2,2)
-polarhistogram(theta,'BinWidth',bindeg*pi/180)
+polarhistogram(theta,'BinEdges',linspace(0,2*pi,binnum))
+% polarhistogram(theta_transform,'BinEdges',linspace(0,2*pi,binnum))
+hold on
+ax = gca;
+polarplot(median(theta(fwd_mask),'omitnan')*[1,1],[0,ax.RLim(2)],'r--')
+polarplot(median(theta(~fwd_mask),'omitnan')*[1,1],[0,ax.RLim(2)],'r--')
+fprintf(strcat("Mean fwd theta (deg): ", num2str(mean(theta(fwd_mask)*180/pi,'omitnan')), "\n"))
 set(gca,'ThetaZeroLocation','top')
 
 % subplot(2,2,3)
