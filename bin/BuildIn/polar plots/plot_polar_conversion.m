4b787cdd5b69287a49ce22b6db11ea38879980ba
Joseph Slivka <slivka@Josephs-MacBook-Pro-2.local>
2024-04-08 09:26:35 -0700

    changed polar plot standards

--- bin/BuildIn/polar plots/plot_polar_conversion.m
+++ bin/BuildIn/polar plots/plot_polar_conversion.m
@@ -18,7 +18,7 @@ end
 
 % plot in cool spherical histogram fashion
 f = figure();
-subplot(1,3,1)
+subplot(1,2,1)
 polarscatter(theta,r)
 set(gca,'ThetaZeroLocation','top')
 
@@ -26,7 +26,7 @@ set(gca,'ThetaZeroLocation','top')
 % theta_transform = theta_transform - pi*(theta_transform > pi);
 fwd_mask = and(theta > -pi/2, theta < pi/2);
 
-subplot(1,3,2)
+subplot(1,2,2)
 polarhistogram(theta,'BinEdges',linspace(0,2*pi,binnum))
 % polarhistogram(theta_transform,'BinEdges',linspace(0,2*pi,binnum))
 hold on
@@ -35,6 +35,7 @@ polarplot(median(theta(fwd_mask),'omitnan')*[1,1],[0,ax.RLim(2)],'r--')
 polarplot(median(theta(~fwd_mask),'omitnan')*[1,1],[0,ax.RLim(2)],'r--')
 fprintf(strcat("Mean fwd theta (deg): ", num2str(mean(theta(fwd_mask)*180/pi,'omitnan')), "\n"))
 set(gca,'ThetaZeroLocation','top')
+set(gca,'fontname','Arial');
 
 % subplot(2,2,3)
 % polarhistogram(theta(r>0),'BinWidth',bindeg*pi/180)
@@ -49,15 +50,18 @@ set(gca,'ThetaZeroLocation','top')
 x = r.*cos(theta);
 y = r.*sin(theta);
 
-
-subplot(1,3,3)
-hist3([y',x'], 'CDataMode','auto','Ctrs', {-20:10:20, -32:2:42});
+fig = figure('Position',[300,300,100,200]);
+% subplot(1,3,3)
+hist3([y',x'], 'CDataMode','auto','Ctrs', {-15:5:15, -28:2:38});
+colormap('hot')
 view(2)
 % imagesc(flipud(hh3))
 ax = gca;
 set(ax,'XDir','reverse')
-% ax.YTick = -40:8:50;
-% ax.XTick = -20:8:20;
+ax.YTick = -24:8:32;
+ax.XTick = -15:10:15;
 % set(ax, 'YTick',[0 yt], 'YTickLabel', [flip([0 yt])]*10)
+fontsize(fig,11,'points');
+set(ax,'fontname','Arial');
 
 end
