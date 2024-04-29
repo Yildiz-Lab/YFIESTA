03ab468c2636e45f7b41bd008422ed016edbaa67
Joseph Slivka <slivka@Josephs-MacBook-Pro-2.local>
2024-04-29 10:35:18 -0700

    more blip analysis changes

--- bin/BuildIn/blip analysis/analyze_blips_wrapper.m
+++ bin/BuildIn/blip analysis/analyze_blips_wrapper.m
@@ -26,6 +26,8 @@ fnum = length(f);
 
 dt = [];
 dx = []; step = [];
+blipin = []; blipout = [];
+totpts = 0; stepnum = 0;
 
 for i=1:fnum
     
@@ -46,9 +48,36 @@ for i=1:fnum
     if isfield(steptrace,'data')
     [dtprime, dxprime, stepprime] = analyze_blips(steptrace.data);
     dt = [dt, dtprime]; dx = [dx, dxprime]; step = [step, stepprime];
+    [bin, bout, pts, num] = analyze_blips_vs(steptrace.data);
+    blipin = [blipin; bin]; blipout = [blipout; bout];
+    totpts = totpts + pts; stepnum = stepnum + num;
+
+    if mean(blipout) < -30
+        fprintf(fname)
+    end
+
     end
 
 end
+
+fprintf(strcat("Prob inside window (window size 3): ", num2str(round(length(blipin)/(3*stepnum),2)),"\n"))
+fprintf(strcat("Size ",num2str(round(-mean(blipin),2))," +/- ",num2str(round(std(blipin),2)),"\n"))
+fprintf(strcat("Prob outside window: ", num2str(round(length(blipout)/(totpts-3*stepnum),2)),"\n"))
+fprintf(strcat("Size ",num2str(round(-mean(blipout),2))," +/- ",num2str(round(std(blipout),2)),"\n"))
+
+ttest2(blipin,blipout)
+
+% cdf
+mdl_exp_cdf = fittype('real(gammainc(k*x,1))','indep','x');
+xcdf = sort(dt(~isnan(dt)));
+ycdf = (1:length(dt(~isnan(dt))))/length(dt(~isnan(dt)));
+cdffit = fit(xcdf',ycdf',mdl_exp_cdf,'start',[0.001])
+
+f0 = figure();
+histogram(blipin,'BinWidth',2,'Normalization','probability','DisplayName','By step')
+hold on
+histogram(blipout,'BinWidth',2,'Normalization','probability','DisplayName','Away from step')
+legend('Location','northwest')
 
 f = figure();
 subplot(1,2,1)
